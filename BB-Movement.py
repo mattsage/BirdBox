419a51076e766e0fb8d600c4fa238459447d5436
Matthew Sage <mattsage@users.noreply.github.com>
2016-06-06 11:55:16 +0100

    Create BB-Movement.py

---
+++ BB-Movement.py
@@ -0,0 +1,22 @@
+#Import Libraries
+import RPi.GPIO as GPIO
+import time
+import picamera
+#import time
+#import datetime
+#import os
+
+#Configure GPIO #19 for LISIPAROI
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(19, GPIO.OUT) 
+
+GPIO.output(19, GPIO.HIGH) # Turn on LEDs
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(5) #sleep 5 seconds
+    camera.capture('/home/pi/Documents/BirdBox/test.jpg') #Take Picture
+    camera.stop_preview() 
+#	os.rename("test.jpg", time.strftime("BirdBox_%Y%m%d%H%M.jpg"))
+
+GPIO.output(19, GPIO.LOW) #Turn off LEDs
