afe9e04d9371e7ba7a5e35177886581e7894b747
mattsage <mattsage@rocketmail.com>
2016-11-20 13:08:12 +0000

    adding scripts

---
+++ bb.py
@@ -0,0 +1,20 @@
+#Import Libraries
+import RPi.GPIO as GPIO 
+import time 
+import picamera
+#import time import datetime import os Configure GPIO #19 for 
+#LISIPAROI
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(17, GPIO.OUT) 
+GPIO.output(17, GPIO.HIGH) # Turn on LEDs 
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(5) #sleep 5 seconds
+    camera.capture('image.jpg') 
+#Take Picture
+    camera.stop_preview()
+#	os.rename("/home/pi/Documents/BirdBox/Motion/image.jpg", 
+#	time.strftime("/home/pi/Documents/BirdBox/Motion/BBM_%Y%m%d%H%M.jpg"))
+GPIO.output(17, GPIO.LOW) #Turn off LEDs
+GPIO.cleanup()
